24bb23f318fede38a73ade29a674c004d95d9644
jakobmerljakmerljak <merljak.jakob@gmail.com>
2021-12-19 23:22:29 +0100

    Make actsub submit multiple jobs

--- src/actsub.py
+++ src/actsub.py
@@ -16,7 +16,7 @@ def main():
     addCommonArgs(parser)
     parser.add_argument('--site', default='default',
             help='site that jobs should be submitted to')
-    parser.add_argument('xRSL', help='path to job description file')
+    parser.add_argument('xRSL', nargs='+', help='path to job description file')
     args = parser.parse_args()
 
     confDict['proxy']  = args.proxy
@@ -27,69 +27,70 @@ def main():
 
     token = readTokenFile(DEFAULT_TOKEN_PATH)
 
-    try:
-        with open(args.xRSL, 'r') as f:
-            xrslStr = f.read()
-    except Exception as e:
-        print('error: xRSL file open: {}'.format(str(e)))
-        sys.exit(1)
-
-    # parse job description and input files first before doing anything else
-    jobdescs = arc.JobDescriptionList()
-    parseResult = arc.JobDescription_Parse(xrslStr, jobdescs)
-    #if not arc.JobDescription_Parse(xrslStr, jobdescs):
-    #    print('error: job description parse error')
-    if not parseResult:
-        sys.exit(1)
-    files = [] # list of tuples of file name and path
-    for infile in jobdescs[0].DataStaging.InputFiles:
-        # TODO: add validation for different types of URLs
-        path = infile.Sources[0].FullPath()
-        if not path:
-            path = infile.Name
-        if not os.path.isfile(path):
+    for desc in args.xRSL:
+        try:
+            with open(desc, 'r') as f:
+                xrslStr = f.read()
+        except Exception as e:
+            print('error: xRSL file open: {}'.format(str(e)))
             continue
-        files.append((infile.Name, path))
 
-    # submit job to receive jobid
-    baseUrl= '{}:{}'.format(confDict['server'], str(confDict['port']))
-    requestUrl = '{}/jobs'.format(baseUrl)
-    jsonDict = {'site': args.site, 'desc': xrslStr}
-    params = {'token': token}
-    try:
-        r = requests.post(requestUrl, json=jsonDict, params=params)
-    except Exception as e:
-        print('error: request: {}'.format(str(e)))
-        sys.exit(1)
-    jsonDict = r.json()
-    if r.status_code != 200:
-        print('error: request response: {} - {}'.format(r.status_code, jsonDict['msg']))
-        sys.exit(1)
-    jobid = jsonDict['id']
+        # parse job description and input files first before doing anything else
+        jobdescs = arc.JobDescriptionList()
+        parseResult = arc.JobDescription_Parse(xrslStr, jobdescs)
+        #if not arc.JobDescription_Parse(xrslStr, jobdescs):
+        #    print('error: job description parse error')
+        if not parseResult:
+            continue
+        files = [] # list of tuples of file name and path
+        for infile in jobdescs[0].DataStaging.InputFiles:
+            # TODO: add validation for different types of URLs
+            path = infile.Sources[0].FullPath()
+            if not path:
+                path = infile.Name
+            if not os.path.isfile(path):
+                continue
+            files.append((infile.Name, path))
 
-    # upload data files for given job
-    requestUrl = '{}/data'.format(baseUrl)
-    params = {'token': token, 'id': jobid}
-    for name, path in files:
-        filesDict = {'file': (name, open(path, 'rb'))}
-        # TODO: handle exceptions
-        r = requests.put(requestUrl, files=filesDict, params=params)
+        # submit job to receive jobid
+        baseUrl= '{}:{}'.format(confDict['server'], str(confDict['port']))
+        requestUrl = '{}/jobs'.format(baseUrl)
+        jsonDict = {'site': args.site, 'desc': xrslStr}
+        params = {'token': token}
+        try:
+            r = requests.post(requestUrl, json=jsonDict, params=params)
+        except Exception as e:
+            print('error: request: {}'.format(str(e)))
+            continue
+        jsonDict = r.json()
         if r.status_code != 200:
-            print('error: unsuccessful upload of file {}: {} - {}'.format(path, r.status_code, r.json()['msg']))
-            # TODO: what to do in such case? Kill job, retry?
-            sys.exit(1)
+            print('error: request response: {} - {}'.format(r.status_code, jsonDict['msg']))
+            continue
+        jobid = jsonDict['id']
 
-    # complete job submission
-    requestUrl = '{}/jobs'.format(baseUrl, jobid)
-    jsonDict = {'id': jobid, 'desc': xrslStr}
-    # TODO: handle exceptions
-    r = requests.put(requestUrl, json=jsonDict, params=params)
-    if r.status_code != 200:
-        print('error: unsuccessful completion of job submission: {} - {}'.format(r.status_code, r.json()['msg']))
-        sys.exit(1)
+        # upload data files for given job
+        requestUrl = '{}/data'.format(baseUrl)
+        params = {'token': token, 'id': jobid}
+        for name, path in files:
+            filesDict = {'file': (name, open(path, 'rb'))}
+            # TODO: handle exceptions
+            r = requests.put(requestUrl, files=filesDict, params=params)
+            if r.status_code != 200:
+                print('error: unsuccessful upload of file {}: {} - {}'.format(path, r.status_code, r.json()['msg']))
+                # TODO: what to do in such case? Kill job, retry?
+                continue
 
-    jobid = r.json()['id']
-    print('{} - succesfully submited job with id {}'.format(r.status_code, jobid))
+        # complete job submission
+        requestUrl = '{}/jobs'.format(baseUrl, jobid)
+        jsonDict = {'id': jobid, 'desc': xrslStr}
+        # TODO: handle exceptions
+        r = requests.put(requestUrl, json=jsonDict, params=params)
+        if r.status_code != 200:
+            print('error: unsuccessful completion of job submission: {} - {}'.format(r.status_code, r.json()['msg']))
+            continue
+
+        jobid = r.json()['id']
+        print('{} - succesfully submited job with id {}'.format(r.status_code, jobid))
 
 
 if __name__ == '__main__':
