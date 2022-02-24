b002e00afe138d570c9c999f12549c7ef4e4b170
jakobmerljakmerljak <merljak.jakob@gmail.com>
2022-02-24 14:41:22 +0100

    Remove unneeded code

--- src/common.py
+++ src/common.py
@@ -6,24 +6,6 @@ import trio
 import httpx
 
 
-def addCommonArgs(parser):
-    parser.add_argument('--server', default=None, type=str,
-            help='URL to aCT server')
-    parser.add_argument('--port', default=None, type=int,
-            help='port on aCT server')
-    parser.add_argument('--conf', default=None, type=str,
-            help='path to configuration file')
-
-
-def addCommonJobFilterArgs(parser):
-    parser.add_argument('-a', '--all', action='store_true',
-            help='all jobs that match other criteria')
-    parser.add_argument('--id', default=None,
-            help='a list of IDs of jobs that should be queried')
-    parser.add_argument('--name', default=None,
-            help='substring that jobs should have in name')
-
-
 def checkJobParams(args):
     if not args.all and not args.id:
         raise ACTClientError("No job ID given (use -a/--all) or --id")
@@ -62,67 +44,12 @@ def checkIDString(listStr):
                 raise ACTClientError(f'Invalid ID: {group}')
 
 
-# duplicated from act.client.proxymgr
-# Since ARC doesn't seem to complain about non certificate files, should we
-# check if given file is actual certificate here?
-def readProxyFile(filename):
-    try:
-        with open(filename, 'r') as f:
-            return f.read()
-    except Exception as e:
-        print(f'error: read proxy: {e}')
-        sys.exit(1)
-
-
-# duplicated from act.client.common
-def showHelpOnCommandOnly(argparser):
-    if len(sys.argv) == 1:
-        argparser.print_help()
-        sys.exit(0)
-
-
-def readTokenFile(tokenFile):
-    try:
-        with open(tokenFile, 'r') as f:
-            return f.read()
-    except Exception as e:
-        print(f'error: read token file: {e}')
-        sys.exit(1)
-
-
 def readFile(filename):
     try:
         with open(filename, 'r') as f:
             return f.read()
     except Exception as e:
         raise ACTClientError(f'Error reading file {filename}: {e}')
-
-
-# optionally accepts session to dcache and closes it afterwards
-#
-# TODO: response from dcache when removing directory is 204. Should we
-#       fix on this or on less than 300?
-async def clean_webdav(conf, args, jobids, dcclient=None):
-    if not jobids:
-        return
-    if args.dcache and args.dcache != 'dcache':
-        dcacheBase = args.dcache
-    else:
-        dcacheBase = conf.get('dcache', None)
-        if dcacheBase is None:
-            return
-
-    print('Cleaning WebDAV directories ...')
-
-    if not dcclient:
-        client = getProxyCertClient(conf['proxy'])
-    else:
-        client = dcclient
-
-    async with trio.open_nursery() as tasks:
-        for jobid in jobids:
-            url = dcacheBase + '/' + str(jobid)
-            tasks.start_soon(webdav_rmdir, client, url)
 
 
 def getProxyCertClient(proxypath):
@@ -152,20 +79,6 @@ def getWebDAVBase(args, conf):
         else:
             webdavBase = args.webdav  # use webdav URL parameter
     return webdavBase
-
-
-async def webdav_rmdir(client, url):
-    headers = {'Accept': '*/*', 'Connection': 'Keep-Alive'}
-    try:
-        resp = await client.delete(url, headers=headers)
-    except httpx.RequestError as e:
-        print(f'request error: {e}')
-        return
-    # TODO: should we rely on 204 and 404 being the only right answers?
-    if resp.status_code == 404:  # ignore, because we are just trying to delete
-        return
-    if resp.status_code >= 300:
-        print(f'error: cannot remove WebDAV directory {url}: {resp.status_code} - {resp.text}')
 
 
 async def runWithSIGINTHandler(program, *args):
